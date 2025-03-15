602bbee6cddc2b77de5327726d03acd579269afd
AnkitMondal-coder <ankitmondal802@gmail.com>
2025-03-15 15:08:21 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -86,7 +86,7 @@ function startListening() {
 }
 
 function sendCommandToBackend(command) {
-    fetch('https://smart-health-ai.onrender.com/api/process', {
+    fetch('https://smart-health-ai.onrender.com/test', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({symptom: command})
